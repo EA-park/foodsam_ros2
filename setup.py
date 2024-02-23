0029c3813517659867647bd3a61da7c059e3b4cf
EA-park <65931894+EA-park@users.noreply.github.com>
2024-02-23 17:47:58 +0900

    Add the url

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='foodsam_ros2',
-    version='0.0'
+    version='0.0',
+    url='https://github.com/EA-park/foodsam_ros2.git@foodsam_pkg',
 )
